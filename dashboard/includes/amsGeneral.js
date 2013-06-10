348021d4aa92f2800b9a2f0a56be952f079f091d
Eliran Eretz-Kedosha <eliranek@zetta7.com>
2013-06-09 19:36:47 -0700

    added ajax function to js file that was overwritten

--- dashboard/includes/amsGeneral.js
+++ dashboard/includes/amsGeneral.js
@@ -316,6 +316,23 @@ function changeEventType(eventValue)
     }
 }
 
+function sendAjaxGetRequest(url, callbackfunc){
+  if (window.XMLHttpRequest){// code for IE7+, Firefox, Chrome, Opera, Safari
+    xmlhttp=new XMLHttpRequest();
+  }else{// code for IE6, IE5
+    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  
+  xmlhttp.onreadystatechange=function()
+  {
+    if (xmlhttp.readyState==4 && xmlhttp.status==200){
+      callbackfunc(xmlhttp.responseText);
+    }
+  }
+  xmlhttp.open("GET",url,true);
+  xmlhttp.send();
+}
+
 /*****PAGE LOAD*****/
 $(document).ready(function(){
     //select an application box
